57e7097e477fd601310b432a6d9cee74c10a7b9a
Jeongsoo Pang <122253772+jpangece@users.noreply.github.com>
2024-02-23 22:09:28 +0900

    Update input_functions.cpp

--- input_functions.cpp
+++ input_functions.cpp
@@ -4,6 +4,7 @@
 
 //Index of the selection of cities during input.
 //0 refers to unselected; 1 refers to the major city selected; 2 refers to other cities selected; 3 refers to infected threshold selected.
+
 int selected_city_input = 0;
 
 void AddString(std::string &str, int &val, unsigned char key)
@@ -12,11 +13,10 @@ void AddString(std::string &str, int &val, unsigned char key)
     val = atoi(str.c_str());
     char temp[256];
 #ifdef _WINDOWS
-	_itoa_s(val, temp, 10);
+    _itoa_s(val, temp, 10);
 #else
-	_itoa(val, temp, 10);
+    _itoa(val, temp, 10);
 #endif // WINDOWS
-
     str = temp;
 }
 
@@ -45,9 +45,9 @@ void KeyboardFuncInputValues(unsigned char key, int x, int y)
             case 2:
                 AddString(other_city_string, other_city_prod_rt, key);
                 break;
-			case 3:
-				AddString(threshold_infection_percent_string, threshold_infection_percent, key);
-				break;
+	    case 3:
+		AddString(threshold_infection_percent_string, threshold_infection_percent, key);
+		break;
             default:
                 break;
         }
@@ -63,10 +63,10 @@ void KeyboardFuncInputValues(unsigned char key, int x, int y)
                 break;
             case 2:
                 RemoveStringOneChar(other_city_string, other_city_prod_rt);
-				break;
-			case 3:
-				RemoveStringOneChar(threshold_infection_percent_string, threshold_infection_percent);
-				break;
+		break;
+	    case 3:
+		RemoveStringOneChar(threshold_infection_percent_string, threshold_infection_percent);
+		break;
             default:
                 break;
         }
@@ -78,55 +78,71 @@ void MouseFuncInputValues(int button, int state, int x, int y)
 {
     if (button == GLUT_LEFT_BUTTON && state == GLUT_UP)
     {
-        if (x >= major_city_input_x1 && x <= major_city_input_x2 && y >= major_city_input_y1 + input_ui_base_y && y <= major_city_input_y2 + input_ui_base_y)
+        if (x >= major_city_input_x1 
+	    && x <= major_city_input_x2 
+	    && y >= major_city_input_y1 + input_ui_base_y 
+	    && y <= major_city_input_y2 + input_ui_base_y)
         {
             selected_city_input = 1;
             current_major_input_color = select_input_color;
-			current_other_input_color = normal_input_color;
-			threshold_infection_input_color = normal_input_color;
+	    current_other_input_color = normal_input_color;
+	    threshold_infection_input_color = normal_input_color;
         }
-        else if (x >= other_city_input_x1 && x <= other_city_input_x2 && y >= other_city_input_y1 + input_ui_base_y && y <= other_city_input_y2 + input_ui_base_y)
+		
+        else if (x >= other_city_input_x1 
+		&& x <= other_city_input_x2 
+		&& y >= other_city_input_y1 + input_ui_base_y 
+		&& y <= other_city_input_y2 + input_ui_base_y)
         {
             selected_city_input = 2;
             current_major_input_color = normal_input_color;
-			current_other_input_color = select_input_color;
-			threshold_infection_input_color = normal_input_color;
+	    current_other_input_color = select_input_color;
+	    threshold_infection_input_color = normal_input_color;
         }
-		else if (x >= threshold_infection_percent_input_x1 && x <= threshold_infection_percent_input_x2 && y >= threshold_infection_percent_input_y1 + input_ui_base_y && y <= threshold_infection_percent_input_y2 + input_ui_base_y)
-		{
-			selected_city_input = 3;
-			current_major_input_color = normal_input_color;
-			current_other_input_color = normal_input_color;
-			threshold_infection_input_color = select_input_color;
-		}
+		
+	else if (x >= threshold_infection_percent_input_x1 
+		&& x <= threshold_infection_percent_input_x2 
+		&& y >= threshold_infection_percent_input_y1 + input_ui_base_y 
+		&& y <= threshold_infection_percent_input_y2 + input_ui_base_y)
+	{
+	    selected_city_input = 3;
+	    current_major_input_color = normal_input_color;
+	    current_other_input_color = normal_input_color;
+	    threshold_infection_input_color = select_input_color;
+	}
+		
         else
         {
             selected_city_input = 4;
             current_major_input_color = normal_input_color;
             current_other_input_color = normal_input_color;
-			threshold_infection_input_color = normal_input_color;
+	    threshold_infection_input_color = normal_input_color;
         }
 
         //The initialization will be done if "OK" button is clicked.
-        if (x >= ok_button_x1 && x <= ok_button_x2 && y >= ok_button_y1 + input_ui_base_y && y <= ok_button_y2 + input_ui_base_y)
+        if (x >= ok_button_x1 
+	    && x <= ok_button_x2 
+	    && y >= ok_button_y1 + input_ui_base_y 
+	    && y <= ok_button_y2 + input_ui_base_y)
         {
             InitializeTime();
-			InitializeCityDatas();
-			InitializeOutlineDatas();
+	    InitializeCityDatas();
+	    InitializeOutlineDatas();
             is_inputed_values = true;
         }
     }
 }
 
-float TriangleArea(std::pair<int, int> p1, std::pair<int, int> p2, std::pair<int, int> p3)//���׹�ʽ
+float TriangleArea(std::pair<int, int> p1, 
+		   std::pair<int, int> p2, 
+		   std::pair<int, int> p3)
 {
-	float AB, BC, AC, P;
-	AB = sqrt(pow(p2.first - p1.first, 2) + pow(p2.second - p1.second, 2));
-	AC = sqrt(pow(p3.first - p1.first, 2) + pow(p3.second - p1.second, 2));
-	BC = sqrt(pow(p3.first - p2.first, 2) + pow(p3.second - p2.second, 2));
-	P = (AB + AC + BC) / 2;
-
-	return sqrt(P*(P - AB)*(P - AC)*(P - BC));
+    float AB, BC, AC, P;
+    AB = sqrt(pow(p2.first - p1.first, 2) + pow(p2.second - p1.second, 2));
+    AC = sqrt(pow(p3.first - p1.first, 2) + pow(p3.second - p1.second, 2));
+    BC = sqrt(pow(p3.first - p2.first, 2) + pow(p3.second - p2.second, 2));
+    P = (AB + AC + BC) / 2;
+    return sqrt(P*(P - AB)*(P - AC)*(P - BC));
 }
 
 bool IsInTriangle(std::pair<int, int> A, std::pair<int, int> B, std::pair<int, int> C, std::pair<int, int> D)
@@ -137,7 +153,7 @@ bool IsInTriangle(std::pair<int, int> A, std::pair<int, int> B, std::pair<int, i
 	S3 = TriangleArea(B, C, D);
 	Ssum = TriangleArea(A, B, C);
 
-	if (0.1 > fabs(Ssum - S1 - S2 - S3))//ע�����ֵ
+	if (0.1 > fabs(Ssum - S1 - S2 - S3))
 		return true;
 	else
 		return false;
@@ -152,18 +168,19 @@ void MouseFuncDialog(int button, int state, int x, int y)
         {
             for (int i = 0; i < city_count; i++)
             {
-					const auto& city = city_infos[i];
-					for (int triangle_i = 0 ; triangle_i < city.city_outline_index.size() ; triangle_i += 3)
-					{
-						auto index_0 = city.city_outline_index[triangle_i];
-						auto index_1 = city.city_outline_index[triangle_i + 1];
-						auto index_2 = city.city_outline_index[triangle_i + 2];
-						if (IsInTriangle(city.city_outline[index_0], city.city_outline[index_1], city.city_outline[index_2], std::make_pair(x, y)))
-						{
-							dialog_city_index = i;
-							return;
-						}
-					}
+		const auto& city = city_infos[i];
+		for (int triangle_i = 0 ; triangle_i < city.city_outline_index.size() ; triangle_i += 3)
+			{
+			    auto index_0 = city.city_outline_index[triangle_i];
+			    auto index_1 = city.city_outline_index[triangle_i + 1];
+			    auto index_2 = city.city_outline_index[triangle_i + 2];
+			    if (IsInTriangle(city.city_outline[index_0], city.city_outline[index_1], 
+				city.city_outline[index_2], std::make_pair(x, y)))
+				{
+			  	    dialog_city_index = i;
+				    return;
+				}
+			}
             }
         }
         else
@@ -177,8 +194,10 @@ void MouseFuncInfo(int button, int state, int x, int y)
 {
     if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
     {
-        if(is_info_shown==0){
-            if(pow(x-info_x,2)+pow(y-info_y,2)<info_R*info_R){
+        if(is_info_shown==0)
+	{
+            if(pow(x-info_x,2)+pow(y-info_y,2)<info_R*info_R)
+	    {
                 is_info_shown=1;
             }
         }
